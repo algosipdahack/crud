2fc34881621d0a5d998bd8cffb677b5fbafaeebb
sophia5460 <sopahia4460@gmail.com>
2021-10-29 15:17:28 +0900

    fix structure

---
+++ db/initdb.d/create_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE users (
+    `id`             INT          NOT NULL,
+    `loginId`       VARCHAR(20)   NOT NULL,
+    `pw`            VARCHAR(100)  NOT NULL,
+    `name`          VARCHAR(20)   NOT NULL,
+    `age`            INT UNSIGNED NOT NULL,
+    `married`         TINYINT     NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE comments (
+    `id`             INT          NOT NULL,
+    `commenter`      INT          NOT NULL,
+    `comment`        VARCHAR(100)      NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (commenter) REFERENCES users (id) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
